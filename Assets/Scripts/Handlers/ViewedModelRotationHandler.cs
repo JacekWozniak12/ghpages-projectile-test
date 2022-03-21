18d0424806f6e7fe95589612ee4f6696c935daba
Jacek Woźniak <jac3kw0zniak@gmail.com>
2022-03-21 01:46:01 +0100

    word fixes

--- Assets/Scripts/Handlers/ViewedModelRotationHandler.cs
+++ Assets/Scripts/Handlers/ViewedModelRotationHandler.cs
@@ -3,43 +3,43 @@ using UnityEngine.EventSystems;
 
 public class ViewedModelRotationHandler : MonoBehaviour
 {
-    public float Sensivity = 25f;
-
-    public void SetSensitivity(float t)
-    {
-        Sensivity = Mathf.Lerp(25, 500, t);
-    }
+    public float Sensitivity = 25f;
+    public void SetSensitivity(float t) => Sensitivity = Mathf.Lerp(25, 500, t);
 
     public void RotateY(PointerEventData pointerEventData)
     {
         Vector3 angles = transform.eulerAngles;
         transform.Rotate(
             0,
-            -pointerEventData.delta.x * Sensivity * Time.deltaTime,
+            -pointerEventData.delta.x * Sensitivity * Time.deltaTime,
             0, Space.World);
     }
+    
     public void RotateXY(PointerEventData pointerEventData)
     {
         Vector3 angles = transform.eulerAngles;
         transform.Rotate(
-            pointerEventData.delta.y * Sensivity * Time.deltaTime,
-            -pointerEventData.delta.x * Sensivity * Time.deltaTime,
+            pointerEventData.delta.y * Sensitivity * Time.deltaTime,
+            -pointerEventData.delta.x * Sensitivity * Time.deltaTime,
             0, Space.World);
     }
+    
     public void RotateX(PointerEventData pointerEventData) => RotateX(pointerEventData.delta.y);
+    
     public void RotateX(float x)
     {
         Vector3 angles = transform.eulerAngles;
         transform.Rotate(
-            x * Sensivity * Time.deltaTime,
+            x * Sensitivity * Time.deltaTime,
             0,
             0, Space.World);
     }
+
     public void RotateXSelf(float x)
     {
         Vector3 angles = transform.eulerAngles;
         transform.Rotate(
-            x * Sensivity * Time.deltaTime,
+            x * Sensitivity * Time.deltaTime,
             0,
             0, Space.Self);
     }
